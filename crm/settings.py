42dda52d14209f1ca54772e9996f501bd1c9f374
Sebastian Jensen <148753669+SebastianFJensen@users.noreply.github.com>
2024-04-16 01:51:10 +0200

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -35,9 +35,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 ALLOWED_HOSTS = []
 
-if 'CODESPACE_NAME' in os.environ:
-    CSRF_TRUSTED_ORIGINS = [f'https://{os.getenv("CODESPACE_NAME")}-8000.{os.getenv("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")}']
-
 # Application definition
 
 INSTALLED_APPS = [
