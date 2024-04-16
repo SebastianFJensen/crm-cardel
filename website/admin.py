02beec64b6f481329281d67eb00fd9ab80e20353
Sebastian Jensen <148753669+SebastianFJensen@users.noreply.github.com>
2024-04-16 13:20:19 +0200

    Update admin.py

--- website/admin.py
+++ website/admin.py
@@ -5,11 +5,13 @@ from .models import Record, Comment, Folder, File
 
 class RecordAdmin(ImportExportModelAdmin):
 	pass
+class CommentAdmin(ImportExportModelAdmin):
+    pass
 
 
 
 admin.site.register(Record,RecordAdmin)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Folder)
 admin.site.register(File)
 path('', include('crm.urls'))
