79a13e76a199fa8c038e7d3dba989dc093cfd5a7
Sebastian Jensen <148753669+SebastianFJensen@users.noreply.github.com>
2024-04-16 12:19:18 +0200

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('Arkiveret/', views.archived, name='archived'),
     path('Prospekter/', views.prospects, name='prospects'),
+    path('Leads/', views.lead, name='lead'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('Record/<int:pk>', views.customer_record, name='Record'),
