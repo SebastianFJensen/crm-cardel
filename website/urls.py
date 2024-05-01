e0532ed41bb05eb416d1a3d7161b9c3724c11d66
Sebastian Jensen <148753669+SebastianFJensen@users.noreply.github.com>
2024-05-01 15:05:15 +0200

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -11,12 +11,12 @@ urlpatterns = [
     path('Leads/', views.lead, name='lead'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
-    path('Record/<int:pk>/', CustomerRecordView.as_view(), name='Record'),
+    path('Record/<int:pk>', views.customer_record, name='Record'),
     path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
     path('add_record/', views.add_record, name='add_record'),
     path('update_record/<int:pk>', views.update_record, name='update_record'),
     path('importrecorddata', views.ImportRecordData.as_view(), name='importrecorddata'),
-    path('Record/<int:pk>/comment/', views.add_comment, name='add_comment1'),
+    path('Record/<int:pk>/comment/', views.AddCommentView.as_view(), name='add_comment1'),
     path('import/', import_from_excel, name='import_from_excel'),
     path('create-folder/', views.create_new_folder, name ='create_new_folder'),
     path('folder/<int:pk>/', views.open_folder, name='open_folder'),
@@ -24,4 +24,4 @@ urlpatterns = [
     path('record/<int:record_id>/comment/<int:comment_id>/edit/', views.EditCommentView.as_view(), name='edit_comment'),
     path('search/', views.search, name='search'),
     path('delete_file/<int:pk>/', views.delete_file, name='delete_file'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
