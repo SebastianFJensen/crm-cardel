77b5791cf8491244f8569e0f85083ddc83b8a597
Sebastian Jensen <148753669+SebastianFJensen@users.noreply.github.com>
2024-05-24 13:35:24 +0200

    Tilføjet kommuner

--- website/admin.py
+++ website/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from import_export.admin import ImportExportModelAdmin
-from .models import Record, Comment, Folder, File, create_folder
+from .models import Record, Comment, Folder, File, create_folder, Almene_kvoter
 from django.db.models.signals import post_save
 
 class RecordAdmin(ImportExportModelAdmin):
@@ -16,9 +16,13 @@ class FolderAdmin(ImportExportModelAdmin):
 class FileAdmin(ImportExportModelAdmin):
     pass
 
+class Almene_kvoterAdmin(ImportExportModelAdmin):
+    pass
+
 admin.site.register(Record, RecordAdmin)
 admin.site.register(Comment, CommentAdmin)
 admin.site.register(Folder, FolderAdmin)
 admin.site.register(File, FileAdmin)
+admin.site.register(Almene_kvoter, Almene_kvoterAdmin)
 
 path('', include('crm.urls'))
